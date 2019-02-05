da39307962c5711d5ad00ccb22e8bc157f254692
Chanon Jenakom <chanonjenakom@gmail.com>
2019-02-05 15:16:44 +0700

    Complete initial clustering API

--- src/clustering/views.py
+++ src/clustering/views.py
@@ -1,6 +1,42 @@
+import json
+from sklearn.cluster import KMeans
+
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
+from .data import document_vectors
+
 @csrf_exempt
 def get_clusters(request):
-    pass
+    if request.method == 'POST':
+        request_body = json.loads(request.body.decode('utf-8'))
+        documents = document_vectors[request_body['from_idx']:request_body['to_idx']]
+
+        clustering = KMeans().fit(documents)
+        clusters_dict = {}
+        for doc_coordinate, label in zip(documents, clustering.labels_):
+            if label not in clusters_dict:
+                clusters_dict[label] = []
+            clusters_dict[label].append(doc_coordinate)
+        
+        clusters = []
+        for idx, label in enumerate(clusters_dict.keys()):
+            size = len(clusters_dict[label])
+            x_list = [doc_coordinate[0] for doc_coordinate in clusters_dict[label]]
+            y_list = [doc_coordinate[1] for doc_coordinate in clusters_dict[label]]
+            x = sum(x_list) / len(x_list)
+            y = sum(y_list) / len(y_list)
+            clusters.append({
+                'id': int(label + 1),
+                'hashtag': int(label + 1),
+                'x': float(x),
+                'y': float(y),
+                'size': int(size)
+            })
+        clusters.sort(key=lambda c: c['id'])
+        
+        return JsonResponse({
+            'clusters': clusters
+        })
+    else:
+        return HttpResponse(status=405)
